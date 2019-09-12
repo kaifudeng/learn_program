5a5cd96cc9c4cd16fc7370bdceff3f0675a07b89
凯夫 邓 <943514827@qq.com>
2019-09-12 17:04:01 +0800

    plus

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 1;
+            x = A.plus(x);
+            Console.WriteLine(x);
+        }
+    }
+    class A
+    {
+        public static int plus(int x)
+        {
+            x += 1;
+            return x;
+        }
+    }
+}
