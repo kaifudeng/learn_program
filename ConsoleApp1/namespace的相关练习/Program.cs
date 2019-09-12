6922a630b557131cc9b37267c7b45e961dd93f19
凯夫 邓 <943514827@qq.com>
2019-09-12 22:03:07 +0800

    main方法部分还有两个未解决的疑问。

---
+++ ConsoleApp1/namespace的相关练习/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Introduction = Wrox.ProCSharp.Basics;
+class Test
+{
+    public static int Main()
+    {
+        Introduction::NamespaceExample nsex =
+            new Introduction::NamespaceExample();
+        Console.WriteLine(nsex.GetNamespace());
+        return 0;
+    }
+}
+namespace Wrox.ProCSharp.Basics
+{
+    class NamespaceExample
+    {
+        public string GetNamespace()
+        {
+            return this.GetType().Namespace;
+        }
+    }
+}
---
+++ ConsoleApp1/多个main方法/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace 多个main方法
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+    }
+    /*class Program2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("not this main!");
+        }
+    }*/
+}
