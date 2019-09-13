759dad1cecac916e2fb62906f54c00066441a305
凯夫 邓 <943514827@qq.com>
2019-09-13 15:38:20 +0800

    抄了书里面有关继承的代码。

---
+++ ConsoleApp1/继承/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace 继承
+{
+    class CustomerAccount
+    {
+        public virtual decimal CalculatePrice()
+        {
+            return 0.0M;
+        }
+    }
+    class GoldAccount:CustomerAccount
+    {
+        public override decimal CalculatePrice()
+        {
+            return base.CalculatePrice()*0.9M;
+        }
+    }
+    abstract class Building //抽象类的构建
+    {
+        public abstract decimal CalculateHeatingCost();
+    }
+
+    public abstract class GenericCustomer
+    {
+        private string name;
+        public GenericCustomer(string name):base()
+        {
+            this.name = name;
+        }
+    }
+    class Nevermore60Customer : GenericCustomer
+    {
+        private int highCostMinutesUsed;
+        private string referrerName;
+        public Nevermore60Customer(string name,string referrername):base(name) 
+        {
+            this.referrerName = referrername;
+        }
+        public Nevermore60Customer(string name):this(name,"<None>")
+        {
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericCustomer customer = new Nevermore60Customer("");
+
+        }
+    }
+}
