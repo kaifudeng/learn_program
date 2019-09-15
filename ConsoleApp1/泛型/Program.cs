cd70942bd4b2515cdd2866f718712a00fcfd7087
凯夫 邓 <943514827@qq.com>
2019-09-15 22:13:17 +0800

    抄了泛型的相关代码，但是完全看不懂

---
+++ ConsoleApp1/泛型/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace 泛型
+{
+   /* public class LinkedListNode //非泛型的链表
+    {
+        public LinkedListNode(object value)
+        {
+            this.Value = value;
+        }
+        public object Value { get; private set; }
+        public LinkedListNode Next { get; internal set; }
+        public LinkedListNode Prev { get; internal set; }
+    }*/
+    public class LinkedListNode<T>
+    {
+        public LinkedListNode(T value)
+            {
+              this.Value=value;
+            }
+    public T Value { get; private set; }
+    public LinkedListNode<T> Next { get; internal set; }
+    public LinkedListNode<T> Prev { get; internal set; }
+    }
+    public class LinkedList<T> : IEnumerable<T>
+    {
+        public LinkedListNode<T>First { get; private set; }
+        public LinkedListNode<T> Last { get; private set; }
+
+        public LinkedListNode<T> AddLast(T node)
+        {
+            var newNode = new LinkedListNode<T>(node);
+            if (First == null)
+            {
+                First = newNode;
+                Last = First;
+            }
+            else
+            {
+                LinkedListNode<T> previous = Last;
+                Last.Next = newNode;
+                Last = newNode;
+                Last.Prev = previous;
+            }
+            return newNode;
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            LinkedListNode<T> current = First;
+            while (current != null)
+            {
+                yield return current.Value;
+                current = current.Next;
+            }
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list2 = new LinkedList<int>();
+            list2.AddLast(1);
+            list2.AddLast(2);
+            list2.AddLast(5);
+
+            foreach (int i in list2)
+            {
+                Console.WriteLine(i);
+            }
+
+            var list3 = new LinkedList<string>();
+            list3.AddLast(2);
+            list3.AddLast("four");
+            list3.AddLast("foo");
+
+            foreach (string s in list3)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/泛型文档管理器/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace 泛型文档管理器
+{
+    public class DocumentManager<T>
+    {
+        
+        private readonly Queue<T> documentQueue = new Queue<T>();
+        public void DisplayAllDocument()
+        {
+            
+            foreach(T doc in documentQueue)
+            {
+
+                Console.WriteLine(((IDocument)doc).Title);
+            }
+        }
+        public void DisplayAllDocuments()
+        {
+            foreach(TDocument doc in documentQueue)
+            {
+                Console.Write(doc.Title);
+            }
+        }
+        public void AddDocument(T doc)
+        {
+            lock (this)
+            {
+                documentQueue.Enqueue(doc);
+            }
+        }
+        public bool IsDocumentAvailable
+        {
+            get { return documentQueue.Count < 0; }
+        }
+        public T GetDocument()
+        {
+            T doc = default(T);
+            lock(this)
+            {
+                doc = documentQueue.Dequeue();
+
+            }
+            return doc;
+        }
+    }
+    public interface IDocument
+    {
+        string Title { get; set; }
+        string Content { get; set; }
+    }
+    public class Document : IDocument
+    {
+        
+    public Document()
+        {
+            
+        }
+        public Document(string title,string content)
+        {
+            this.Title = title;
+            this.Content = content;
+        }
+        public string Title { get; set; }
+        public string Content { get; set; }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dm = new DocumentManager<Document>();
+            dm.AddDocument(new Document("Title A","Sample A"));
+            dm.AddDocument(new Document("Title B", "Sample B"));
+            dm.DisplayAllDocuments();
+            if (dm.IsDocumentAvailable)
+            {
+                Document d = dm.GetDocument();
+                Console.WriteLine(d.Content);
+            }
+
+        }
+    }
+}
