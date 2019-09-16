7351e724e3f339e06644d6341300b3d7abbadd5a
凯夫 邓 <943514827@qq.com>
2019-09-16 21:03:33 +0800

    泛型的章节终于写完了！好难啊

---
+++ ConsoleApp1/泛型方法/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace 泛型方法
+{
+    public class Account
+    {
+        public string Name { get; private set; }
+        public decimal Balance { get; private set; }
+        public Account(string name,Decimal balance)
+        {
+            this.Name = name;
+            this.Balance = balance;
+        }
+    }
+    
+    class Program
+    {
+       public void Swap<T>(ref T x, ref T y)
+        {
+            T temp;
+            temp = x;
+            x = y;
+            y = temp;
+        }
+        /*public static decimal Accumulate<TAccount>(IEnumerable<TAccount> source)
+        where TAccount:IAccount
+        {
+            decimal sum = 0;
+            foreach(TAccount a in source)
+            {
+                sum += a.balance;
+            }
+            return sum;
+
+        }
+        public class Account:IAccount
+        { }
+        public interface IAccount
+        {
+            decimal balance { get; }
+            string name { get; }
+        }
+        */
+        public class MethodOverloads
+        {
+            public void foo<T>(T obj)
+            {
+                Console.WriteLine("Foo<T>(T obj), obj type:{0}", obj.GetType().Name);
+            }
+            public void foo(int x)
+            {
+                Console.WriteLine("Foo(int x)");
+            }
+            public void Bar<T>(T obj)
+            {
+                foo(obj);
+            }
+        }
+        static void Main(string[] args)
+        {
+            int i, j;
+            i = 4;
+            j = 5;
+            //Swap(ref i, ref j);
+
+            
+
+            var test = new MethodOverloads();
+            test.foo(300);
+            test.foo("abc");
+            test.Bar(44);
+
+        }
+    }
+}
---
+++ ConsoleApp1/泛型结构/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace 泛型结构
+{
+    public struct Nullable<T>
+    
+        where T:struct
+   {
+        public Nullable(T value)
+        {
+            this.hasvalue = true;
+            this.value = value;
+        }
+        private bool hasvalue;
+        public bool Hasvalue
+        {
+            get
+            {
+                return hasvalue;
+            }
+
+        }
+        private T value;
+        public T Value
+        {
+            get
+            {
+                if (!hasvalue)
+                {
+                    throw new InvalidOperationException("no Value");
+                }
+                return value;
+            }
+        }
+        public static explicit operator T(Nullable<T> value)
+        {
+            return value.Value;
+        }
+        public static implicit operator Nullable<T>(T value)
+        {
+            return new Nullable<T>(value);
+        }
+        public override string ToString()
+        {
+            if (!hasvalue)
+            
+                return String.Empty;
+                return this.value.ToString();
+            
+        }
+   }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Nullable<int> x;
+            x = 4;
+            x  =x+3;
+            if (x.Hasvalue)
+            {
+                int y = x.Value;
+            }
+            x = null;
+            int? x1;
+            x1 = null;
+        }
+    }
+}
