5e3f7e4353fa0d1c24df17c3bbc4eb19eb7efb30
凯夫 邓 <943514827@qq.com>
2019-09-16 16:04:12 +0800

    add 泛型继承部分的代码。

---
+++ ConsoleApp1/泛型的继承/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace 泛型的继承
+{
+    public abstract class Calc<T>
+    {
+        public abstract T Add(T x, T y);
+        public abstract T Sub(T x, T y);
+    }
+    public class IntCalc:Calc<int>
+    {
+        public override int Add(int x,int y)
+        { return x + y; }
+        public override int Sub(int x,int y)
+        { return x - y; }
+    }
+    public class StaticDemo<T>
+    { public static int x; }
+
+    class Program
+    {
+        
+            
+        static void Main(string[] args)
+        {
+            StaticDemo<int>.x = 5;
+            StaticDemo<string>.x = 4;
+            Console.WriteLine(StaticDemo<int>.x);
+            Console.WriteLine(StaticDemo<string>.x);
+        }
+    }
+}
