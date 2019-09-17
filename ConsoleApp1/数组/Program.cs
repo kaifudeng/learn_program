feb1ba335bedce4716e0d7475707a84c589f8156
凯夫 邓 <943514827@qq.com>
2019-09-18 01:06:31 +0800

    抄写了书里复制数组的代码，有些内容比较迷惑。

--- ConsoleApp1/数组/Program.cs
+++ ConsoleApp1/数组/Program.cs
@@ -1,9 +1,20 @@
 ﻿using System;
 
+
 namespace 数组
 {
-    public class Person
+    public class Person:IComparable<Person>
     {
+        public int CompareTo(Person other)
+            {
+              if(other==null)return 1;
+              int result=string.Compare(this.LastName,other.LastName);
+              if(result==0)
+              {
+                result=string.Compare(this.FirstName,other.FirstName);
+              }
+              return result;
+            }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public override string ToString()
@@ -11,6 +22,9 @@ namespace 数组
             return String.Format("{0}  {1}",FirstName,LastName);
         }
     }
+    public interface IComparable<Person>
+        {CompareTo();
+}
     class Program
     {
         static void Main(string[] args)
@@ -45,6 +59,70 @@ namespace 数组
             int[] arr2 = { 4, 5, 6 };
             Array racers = Array.CreateInstance(typeof(Person),arr1,arr2);
 
-        }
+            //Setvalue（）方法
+            racers.SetValue(new Person
+                {
+                FirstName="Alain",
+                LastName="Prost"
+                },index1:1,index2:10);
+            racers.SetValue(new Person
+                {
+                FirstName="Emerson",
+                LastName="Fittipaldi"
+                },1,11);
+            racers.SetValue(new Person
+                {
+                FirstName="Ayrton",
+                LastName="Senna"},1,12);
+            racers.SetValue(new Person
+                {
+                FirstName="Michael",
+                LastName="Schumacher"},2,10);
+            racers.SetValue(new Person
+                {
+                FirstName="Fernando",
+                LastName ="Alonso"},2,11);
+
+            Person[,] racers2=(Person[,])racers; 
+            Person First=racers2[1,10];
+            Person Last=racers2[2,12];
+
+            //复制数组
+            int[] intArray1={1,2};
+            int[] intArray2=(int[])intArray1.clone();
+
+            Person[] beatles={
+                                              new Person{FirstName="John",LastName="Lennon"},
+                                              new Person{FirstName="Paul",LastName="McCartney"}
+                                        };
+            Person[] beatlesClone=(Person[])beatles.clone();
+
+            //数组排序
+            string[] names=
+                {
+                  "AAA",
+                  "BBB",
+                  "CCC",
+                  "DDD"
+                };
+            Array.Sort(names);
+            foreach(var name in names)
+                {
+                Console.WriteLine(name);
+                }
+
+            Person[] persons=
+                {
+                new Person{FirstName="AAA",LastName="aaa"},
+                new Person{FirstName="BBB",LastName="bbb"},
+                new Person{FirstName="DDD",LastName="ddd"},
+                new Person{FirstName="CCC",LastName="ccc"}
+                };
+            Array.Sort(persons);
+            foreach(var p in persons)
+                {
+                Console.WriteLine(p);
+                }
     }
+}
 }
