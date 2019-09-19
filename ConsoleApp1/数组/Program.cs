d0e4730d0be30d45d67b254b0f010a49a9ac36c6
凯夫 邓 <943514827@qq.com>
2019-09-19 10:18:47 +0800

    抄完了数组这一章节的全部代码，枚举的部分有很多错误没有解决…

--- ConsoleApp1/数组/Program.cs
+++ ConsoleApp1/数组/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 
 
 namespace 数组
@@ -23,12 +24,19 @@ namespace 数组
         }
     }
     public interface IComparable<Person>
-        {CompareTo();
+        {public int CompareTo();
 }
     class Program
     {
         static void Main(string[] args)
         {
+            //GameMoves的测试
+            var game = new GameMoves();
+            IEnumerator enumerator = game.Cross();
+            while (enumerator.MoveNext())
+            {
+                enumerator = enumerator.Current as IEnumerator;
+            }
             Person[] myperson = new Person[2];
             myperson[0] = new Person { FirstName = "oula!", LastName = "muda!" };
             myperson[1] = new Person { FirstName = "jotairo", LastName = "dio" };
@@ -123,6 +131,168 @@ namespace 数组
                 {
                 Console.WriteLine(p);
                 }
+            static int SumOfSegments(ArraySegment<int>[] segments)
+            {
+                int sum = 0;
+                foreach(var segment in segments)
+                {
+                    for(int i = segment.Offset; i < segment.Offset + segment.Count; i++)
+                    {
+                        sum += segment.Array[i];
+                    }
+                }
+                return sum;
+            }
+            int[] ar1 = { 1, 4, 5, 11, 13, 18 };
+            int[] ar2 = { 3, 4, 5, 18, 21, 27 };
+            var segments = new ArraySegment<int>(2)
+            {
+                new ArraySegment<int>(ar1,0,3),
+                new ArraySegment<int>(ar2,3,3)
+            };
+            //枚举的部分
+            IEnumerator<Person> enumerator = persons.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                Person p = enumerator.Current;
+                Console.WriteLine(p);
+            }
+
     }
+        static Person[] GetPerson()
+        {
+            return new Person[]
+            {
+                new Person{FirstName="dodo",LastName="dori"},
+                new Person{FirstName="shit",LastName="act3"},
+                new Person{FirstName="star",LastName="sign"}
+            };
+        }
+        public class HelloCollection
+        {
+            /*public IEnumerator<string> GetEnumerator()
+            {
+                yield return "Hello";
+                yield return "World";
+            }*/
+            public IEnumerator GetEnumerator()
+            {
+                return new Enumerator(0);
+            }
+            public class Enumerator:IEnumerator<string>,IEnumerator,IDisposable
+            {
+                private int state;
+                private string current;
+
+                public Enumerator(int state)
+                {
+                    this.state = state;
+                }
+                bool IEnumerator.MoveNext()
+                {
+                    switch (state)
+                    {
+                        case 0:
+                            current = "Hello";
+                            state = 1;
+                            return true;
+                        case 1:
+                            current = "World";
+                            state = 2;
+                            return true;
+                        case 2:
+                            break;
+                    }
+                    return false;
+                }
+                void IEnumerator.Reset()
+                {
+                    throw new NotSupportedException();
+                }
+                string IEnumerator<string>.current
+                {
+                    get { return current; }
+                }
+                object IEnumerator.Current
+                {
+                    get { return current; }
+                }
+                void IDisposable.Dispose()
+                { }
+            }
+        }
+        public void HelloWorld()
+        {
+            var hellocollection = new HelloCollection();
+            foreach (var s in hellocollection)
+            {
+                Console.WriteLine(s);
+            }
+        }
+        public class MusicTitle
+        {
+            string[] names =
+            {
+                "Tubular Bells","Hergest Ridge","Ommadawn",
+                "Platinum"
+            };
+            public IEnumerator<string> GetEnumerator()
+            {
+                for(int i=0;i<4 ;i++ )
+                {
+                    yield return names[i];
+                }
+            }
+            public IEnumerator<string> Reverse()
+            {
+                for(int i=3;i>=0;i--)
+                {
+                    yield return names[i];
+                }
+            }
+            public IEnumerable<string> Subset(int index,int length)
+            {
+                for(int i=index;i<index+length;i++)
+                {
+                    yield return names[i];
+                }
+            }
+
+        }
+        //yeild语句的练习
+        public class GameMoves
+        {
+            private IEnumerator cross;
+            private IEnumerator circle;
+            public GameMoves()
+            {
+                cross = Cross();
+                circle = Circle();
+            }
+            private int move = 0;
+            const int MaxMove = 9;
+            public IEnumerator Cross()
+            {
+                while (true)
+                {
+                    Console.WriteLine("Cross, Move{0}", move);
+                    if(++move>=MaxMove)
+                        yield break;
+                    yield return circle;
+                }
+            }
+            public IEnumerator Circle()
+            {
+                while(true)
+                {
+                    Console.WriteLine("Circle Move{0}",circle);
+                    if(++move>=MaxMove)
+                    {
+                        yield break;
+                    }
+                    yield return cross;
+                }
+            }
+        }
 }
 }
