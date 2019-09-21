168e9e6acd644ec1dcaa5fde5889ae0fdb5bced4
凯夫 邓 <943514827@qq.com>
2019-09-21 16:15:26 +0800

    添加了一小段运算符重载的代码，有点醍醐灌顶的感觉。

---
+++ Csharp高级编程第七章到第八章部分/运算符重载/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace 运算符重载
+{
+    class Program
+    {
+        struct Vector
+        {
+            public double x, y, z;
+            public Vector(double x,double y,double z)
+            {
+                this.x = x;
+                this.y = y;
+                this.z = z;
+            }
+            public Vector(Vector rhs)
+            {
+                x = rhs.x;
+                y = rhs.y;
+                z = rhs.z;
+            }
+            public override string ToString()
+            {
+                return "(" + x + "," + y + "," + z + ")";
+            }
+            public static Vector operator +(Vector lhs,Vector rhs)
+            {
+                Vector result = new Vector(rhs);
+                result.x += lhs.x;
+                result.y += lhs.y;
+                result.z += lhs.z;
+                return result;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Vector v1, v2, v3;
+            v1 = new Vector(3.0, 3.0, 1.0);
+            v2 = new Vector(2.0, -4.0, -4.0);
+            v3 = v1 + v2;
+            Console.WriteLine("v1=" + v1.ToString());
+            Console.WriteLine("v2=" + v2.ToString());
+            Console.WriteLine("v3=" + v3.ToString());
+        }
+    }
+}
