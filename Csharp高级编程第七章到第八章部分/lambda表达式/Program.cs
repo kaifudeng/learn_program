31a5864e6e33cc639fd044f64fa2effe58056d37
凯夫 邓 <943514827@qq.com>
2019-09-25 00:19:38 +0800

    P207

---
+++ Csharp高级编程第七章到第八章部分/lambda表达式/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lambda表达式
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mid = ",middle part";
+            Func<string, string> lambda = param =>
+               {
+                   param += mid;
+                   param += " and this was added to the string";
+                   return param;
+               };
+            Console.WriteLine(lambda("start of string"));
+
+            //使用foreach语句的闭包
+            var values = new List<int>() { 10, 20, 30 };
+            var funcs = new List<Func<int>>();
+            foreach(var val in values)
+            {
+                funcs.Add(()=>val);
+            }
+            foreach(var f in funcs)
+            {
+                Console.WriteLine(f());
+            }
+        }
+    }
+}
---
+++ Csharp高级编程第七章到第八章部分/匿名方法/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace 匿名方法
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mid = ",middle part,";
+            Func<string, string> anonDel = delegate (string param)
+               {
+                   param += mid;
+                   param += "and this was added to the string.";
+                   return param;
+               };
+            Console.WriteLine(anonDel("Start of string"));
+        }
+    }
+}
