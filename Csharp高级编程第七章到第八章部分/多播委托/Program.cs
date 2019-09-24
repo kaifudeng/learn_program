4a02ec9e77ea148dc5f0a841573effa6dfd71b9c
凯夫 邓 <943514827@qq.com>
2019-09-24 23:55:40 +0800

    写了多播委托的代码，但是不是很懂有什么用

--- Csharp高级编程第七章到第八章部分/多播委托/Program.cs
+++ Csharp高级编程第七章到第八章部分/多播委托/Program.cs
@@ -32,6 +32,33 @@ namespace 多播委托
             ProcessAndDisplayNumber(operations, 7.94);
             ProcessAndDisplayNumber(operations, 1.414);
             Console.WriteLine();
+
+            //P202页代码
+            Action d1 = one;
+            d1 += two;
+            Delegate[] delegates = d1.GetInvocationList();
+            foreach (Action d in delegates)
+            {
+                try
+                {
+                    d();
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Exception caught");
+                }
+            }
+        }
+        //P201页 错误示范代码
+        static void one()
+        {
+            Console.WriteLine("one");
+            throw new Exception("Error in one");
+        }
+        static void two()
+        {
+            Console.WriteLine("two");
+
         }
     }
 }
