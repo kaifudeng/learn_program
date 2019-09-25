15141156ffe9f3533bb831c419f2a3e3d466c692
凯夫 邓 <943514827@qq.com>
2019-09-26 01:11:38 +0800

    完成了第九章的内容，但是正则表达式很难

---
+++ Csharp高级编程第九章到第十章内容/Csharp高级编程第九章到第十章内容/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Text;
+namespace Csharp高级编程第九章到第十章内容
+{
+    public struct Vector : IFormattable
+    {
+        public double x, y, z;
+        public Vector(double x, double y, double z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+        public double Norm()
+        {
+            return x * x + y * y + z * z;
+        }
+        public string ToString(string format, IFormatProvider formatProvider)
+        {
+            if (format == null)
+            {
+                return ToString();
+            }
+            string formatUpper = format.ToUpper();
+            switch (formatUpper)
+            {
+                case "N":
+                    return "| | " + Norm().ToString() + "| |";
+                case "VE":
+                    return string.Format("({0:E},{1:E},{2:E}", x, y, z);
+                case "IJK":
+                    StringBuilder sb = new StringBuilder(x.ToString(), 30);
+                    sb.AppendFormat(" i + ");
+                    sb.AppendFormat(y.ToString());
+                    sb.AppendFormat(" j + ");
+                    sb.AppendFormat(z.ToString());
+                    sb.AppendFormat(" k");
+                    return sb.ToString();
+                default:
+                    return ToString();
+            }
+        }
+        public override string ToString()
+        {
+            return "(" + x + "," + y + "," + z + ")";
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = "taday is a";
+            str1 += "beautiful day";
+
+            for (int i = 'z'; i >= 'a'; i--)
+            {
+                char old1 = (char)i;
+                char new1 = (char)(i + 1);
+                str1 = str1.Replace(old1, new1);
+            }
+            for (int i = 'Z'; i <= 'A'; i--)
+            {
+                char old1 = (char)i;
+                char new1 = (char)(i + 1);
+                str1 = str1.Replace(old1, new1);
+            }
+            Console.WriteLine("Encoded:\n " + str1);
+            Console.WriteLine();
+
+            //P218 stringbuilder类
+            StringBuilder stringA =
+                new StringBuilder("Hello from all the guys at Wrox Press.", 150);//150是初始容量
+            stringA.AppendFormat("we do hope you enjoy this book as much as we " + "enjoyed write it");
+            Console.WriteLine("Not Encoded:\n" + stringA);
+            for (int i = 'z'; i >= 'a'; i--)
+            {
+                char old1 = (char)i;
+                char new1 = (char)(i + 1);
+                stringA = stringA.Replace(old1, new1);
+            }
+            for (int i = 'Z'; i >= 'A'; i--)
+            {
+                char old1 = (char)i;
+                char new1 = (char)(i + 1);
+                stringA = stringA.Replace(old1, new1);
+            }
+            Console.WriteLine("Encoded：\n" + stringA);
+            Console.WriteLine();
+
+            //P226页
+            Vector v1 = new Vector(1, 32, 5);
+            Vector v2 = new Vector(845, 54.3, -7.8);
+            Console.WriteLine("\nIn IJK format,\nl is {0,30:IJK}\nv2 is {1,30:IJK}", v1, v2);
+            Console.WriteLine("\nIn default format,\nv1 is {0,30}\nv2 is {1,30}", v1, v2);
+            Console.WriteLine("\nIn VE format,\nv1 is {0,30:VE}\nv2 is {1,30:VE}", v1, v2);
+            Console.WriteLine("\nNorm are:\nv1 is {0,20:N}\nv2 is {1,20:N}", v1, v2);
+
+        }
+    }
+}
---
+++ Csharp高级编程第九章到第十章内容/正则表达式/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace 正则表达式
+{
+    class Program
+    {
+        //P231
+        static void WriteMatchs(string text,MatchCollection matches)
+        {
+            Console.WriteLine("text was: \n\n" + text + "\n");
+            Console.WriteLine("No. of matches: " + matches.Count);
+
+            foreach(Match next in matches)
+            {
+                int index = next.Index;
+                string result = next.ToString();
+                int charsBefore = (index < 5) ? index : 5;
+                int fromEnd = text.Length - index - result.Length;
+                int charsAfter = (fromEnd < 5 ) ? fromEnd : 5;
+                int charsToDisplay = charsBefore + charsAfter + result.Length;
+                Console.WriteLine("index:{0},\tString:{1},\t{2}",index,result,text.Substring(index-charsBefore,charsToDisplay));
+
+            }
+        }
+        static void find()
+        {
+            string text = @"this comprehensive compendium provides a broad and thorough investigation of all aspects of programming
+        with ASP.NET. Entirely revised and updated for the fourth release of .NET,this book will give you the information
+        you need to master ASP.NET and build a dynamic,successful,enterprise Web application.";
+            string p = @"\ba";
+            MatchCollection matches = Regex.Matches(text, p, RegexOptions.IgnoreCase);
+            WriteMatchs(text, matches);
+        }
+        //P232
+        static void find2()
+        {
+            string text = @"http://www.wrox.com:4355";
+            string p = @"\b(\S+)://([^:]+)(?::(\S))?\b";
+            MatchCollection matches = Regex.Matches(text, p, RegexOptions.IgnoreCase);
+            WriteMatchs(text, matches);
+        }
+
+        static void Main(string[] args)
+        {
+            const string myText =
+        @"this comprehensive compendium provides a broad and thorough investigation of all aspects of programming
+        with ASP.NET. Entirely revised and updated for the fourth release of .NET,this book will give you the information
+        you need to master ASP.NET and build a dynamic,successful,enterprise Web application.";
+            const string pattern = "ion";
+            MatchCollection myMatches = Regex.Matches(myText, pattern, 
+                RegexOptions.IgnoreCase | RegexOptions.ExplicitCapture);
+            foreach(Match nextmatch in myMatches)
+            {
+                Console.WriteLine(nextmatch.Index);
+            }
+            const string pat2 = @"\bn";
+            MatchCollection myMatches2 = Regex.Matches(myText, pat2,
+                RegexOptions.IgnoreCase | RegexOptions.ExplicitCapture);
+
+            Console.WriteLine();
+            //P231
+            find();
+            Console.WriteLine();
+            find2();
+        }
+    }
+}
