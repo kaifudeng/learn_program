f9649869c81ff332b7b50211f7c49da301bfb4b7
凯夫 邓 <943514827@qq.com>
2019-09-27 15:54:05 +0800

    列表的增删改查方法

--- Csharp高级编程第九章到第十章内容/列表/Program.cs
+++ Csharp高级编程第九章到第十章内容/列表/Program.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 
 namespace 列表
 {
+    
     public class Racer:IComparable<Racer>,IFormattable
     {
         public int Id { get; private set; }
@@ -59,13 +63,132 @@ namespace 列表
             return compare;
             
         }
+       
+    }
+
+    public class FindCountry
+    {
+        public FindCountry(string country)
+        {
+            this.country = country;
+        }
+        private string country;
+        public bool findcountryPredicate(Racer racer)
+        {
+            Contract.Requires<ArgumentNullException>(racer != null);
+            return racer.Country==country;
+        }
+    }
+    public class RacerComparer : IComparer<Racer>
+    {
+        public enum ComparerType
+        {
+            FirstName,
+            LastName,
+            Country,
+            Wins
+        }
+        private ComparerType comparerType;
+        public RacerComparer(ComparerType comparerType)
+        {
+            this.comparerType = comparerType;
+        }
+        public int Compare(Racer x,Racer y)
+        {
+            if (x == null && y == null) return 0;
+            if (x == null) return 1;
+            if (y == null) return 1;
+            int result;
+            switch (comparerType)
+            {
+                case ComparerType.FirstName:
+                    return string.Compare(x.FirstName, y.FirstName);
+                case ComparerType.LastName:
+                    return string.Compare(x.LastName, y.LastName);
+                case ComparerType.Country:
+                    result = string.Compare(x.Country, y.Country);
+                    if (result == 0)
+                    {
+                        return string.Compare(x.LastName, y.LastName);
+                    }
+                    else return result;
+                case ComparerType.Wins:
+                    return x.Wins.CompareTo(y.Wins);
+                default:throw new ArgumentException("Invaild Compare Type");
+            }
+        }
+    }
+
+    public class Person
+    {
+        private string name;
+        public Person(string name)
+        {
+            this.name = name;
+        }
+        public override string ToString()
+        {
+            return name;
+        }
     }
     class Program
     {
 
         static void Main(string[] args)
         {
-           
+            var grham = new Racer(7, "Grham", "Hill","UK",14);
+            var emerson = new Racer(13, "Emerson", "Fittipaldi", "Brazil", 14);
+            var mario = new Racer(16, "Mario", "Andretti", "USA", 12);
+            var racers = new List<Racer>(20) { grham, emerson, mario };
+            racers.Add(new Racer(24, "Michael", "Schumacher", "Germany", 91));
+            racers.Add(new Racer(27, "mika", "Hakkinen", "Finland", 20));
+
+            racers.AddRange(new Racer[]
+            {
+                new Racer(14,"niki","lauda","austria",25),
+                new Racer(21,"alain","prost","france",51)
+            });
+
+            racers.Insert(3, new Racer(6, "Phil", "Hill", "USA", 3));
+
+            Racer r1 = racers[3];
+
+            for (int i = 0; i < racers.Count; i++)
+            {
+                Console.WriteLine(racers[i]);
+            }
+            foreach(Racer r in racers)
+            {
+                Console.WriteLine(r);
+            }
+            //删除
+            if(!racers.Remove(grham))
+            {
+                Console.WriteLine("object not found in collection");
+            }
+            racers.RemoveRange(1,2);
+
+            //搜索
+            int index1 = racers.IndexOf(mario);
+            int index2 = racers.FindIndex(new FindCountry("Finland").findcountryPredicate);
+            int index3 = racers.FindIndex(r => r.Country == "finland");
+            List<Racer> Rlist = racers.FindAll(r => r.Wins > 20);
+            foreach (Racer a in racers)
+            {
+                Console.WriteLine("{0:A}", a);
+            }
+
+            //排序
+            racers.Sort();
+            racers.ForEach(Console.WriteLine);
+
+            racers.Sort(new RacerComparer(RacerComparer.ComparerType.Country));
+            racers.ForEach(Console.WriteLine);
+
+            //格式转换
+            List<Person> people = racers.ConvertAll<Person>(r => new Person(r.FirstName + " " + r.LastName));
         }
+        
+        
     }
 }
