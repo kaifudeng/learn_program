741bc51a2733cea0aaca42ce4aef18f1dc9470b6
凯夫 邓 <943514827@qq.com>
2019-10-16 17:30:24 +0800

    写了Entity Framework的部分代码

---
+++ EntityFrameworkSamples/test1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test1
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            DimArea dimArea = new DimArea();
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            using(var data =new BIDWDemoEntities())
+            {
+                foreach (var area in data.DimHour)
+                {
+                    Console.WriteLine(area.HourKey);
+                }
+            }
+            Console.Read();
+        }
+    }
+    
+}
