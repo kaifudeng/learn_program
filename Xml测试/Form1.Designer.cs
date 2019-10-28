bdd9f16d48bd2bf8c9228536cd29195e50151822
凯夫 邓 <943514827@qq.com>
2019-10-28 20:42:02 +0800

    继续写了Xml相关的一些代码

--- Xml测试/Form1.Designer.cs
+++ Xml测试/Form1.Designer.cs
@@ -34,6 +34,9 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -94,11 +97,44 @@
             this.button5.UseVisualStyleBackColor = true;
             this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(1089, 124);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(100, 77);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "write";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(925, 71);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(141, 71);
+            this.button7.TabIndex = 9;
+            this.button7.Text = "XMLDOM";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(1089, 53);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(100, 77);
+            this.button8.TabIndex = 10;
+            this.button8.Text = "XPath";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1201, 450);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -120,6 +156,9 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
     }
 }
 
--- Xml测试/Form1.cs
+++ Xml测试/Form1.cs
@@ -9,6 +9,8 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Xml;
 using System.IO;
+using System.Xml.Schema;
+
 namespace Xml测试
 {
     public partial class Form1 : Form
@@ -112,9 +114,116 @@ namespace Xml测试
             }
         }
 
+        void settings_ValidationEventHandler(object sender, System.Xml.Schema.ValidationEventArgs e)
+        {
+            MessageBox.Show(e.Message);
+        }
+
         private void button5_Click(object sender, EventArgs e)
         {
+            richTextBox1.Clear();
+            XmlReaderSettings settings = new XmlReaderSettings();
+            settings.Schemas.Add(null, "books.xsd");
+            settings.ValidationType = ValidationType.Schema;
+            settings.ValidationEventHandler +=
+                new System.Xml.Schema.ValidationEventHandler(settings_ValidationEventHandler);
+            XmlReader rdr = XmlReader.Create("books.xml",settings);
+            while (rdr.Read())
+            {
+                if (rdr.NodeType == XmlNodeType.Text)
+                {
+                    richTextBox1.AppendText(rdr.Value + "\r\n");
+                }
+            }
 
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            /*XmlWriterSettings settings = new XmlWriterSettings();
+            settings.Indent = true;
+            settings.NewLineOnAttributes = true;
+            XmlWriter writer = XmlWriter.Create("newbook.xml", settings);
+            writer.WriteStartDocument();
+
+            writer.WriteStartElement("book");
+            writer.WriteAttributeString("genre", "Mystery");
+            writer.WriteAttributeString("publicationdate", "2001");
+            writer.WriteAttributeString("ISBN", "123456789");
+            writer.WriteElementString("title", "Case of the Missing Cookie");
+            writer.WriteStartElement("author");
+            writer.WriteElementString("name", "Cookie Monster");
+            writer.WriteEndElement();
+            writer.WriteElementString("Price", "9.99");
+            writer.WriteEndElement();
+            
+
+            writer.Flush();
+            writer.Close();*/
+
+
+            //doc.Load("books.xml");
+
+            XmlDeclaration newDec = doc.CreateXmlDeclaration("1.0", null, null);
+            doc.AppendChild(newDec);
+            XmlElement newRoot = doc.CreateElement("newBookstore");
+            doc.AppendChild(newRoot);
+            XmlElement newbook = doc.CreateElement("book");
+            newbook.SetAttribute("genre", "Mystery");
+            newbook.SetAttribute("publicationdate", "2001");
+            newbook.SetAttribute("ISBN", "123456789");
+
+            XmlElement newTitle = doc.CreateElement("title");
+            newTitle.InnerText = "Case of the Missing Cookie";
+            newbook.AppendChild(newTitle);
+
+            XmlElement newAuthor = doc.CreateElement("author");
+            newbook.AppendChild(newAuthor);
+
+            XmlElement newName = doc.CreateElement("Name");
+            newName.InnerText = "Cookie Monster";
+            newAuthor.AppendChild(newName);
+
+            XmlElement newPrice = doc.CreateElement("price");
+            newPrice.InnerText = "9.95";
+            newbook.AppendChild(newPrice);
+
+            doc.DocumentElement.AppendChild(newbook);
+
+            XmlTextWriter tr = new XmlTextWriter("booksEdit.xml", null);
+            tr.Formatting = Formatting.Indented;
+            doc.WriteContentTo(tr);
+            tr.Close();
+
+            XmlNodeList nodeList = doc.GetElementsByTagName("title");
+            richTextBox1.Text = "";
+            foreach(XmlNode x in nodeList)
+            {
+                richTextBox1.Text += x.OuterXml + "\r\n";
+            }
+        }
+
+        private XmlDocument doc = new XmlDocument();
+        private void button7_Click(object sender, EventArgs e)
+        {
+            doc.Load("books.xml");
+            XmlNodeList nodeList = doc.GetElementsByTagName("title");
+            richTextBox1.Text = "";
+            foreach(XmlNode node in nodeList)
+            {
+                richTextBox1.Text += node.OuterXml + "\r\n";
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            doc.Load("books.xml");
+            XmlNodeList nodeList = doc.SelectNodes("/bookstore/book/title");
+            richTextBox1.Text = "";
+            foreach(XmlNode x in nodeList)
+            {
+                richTextBox1.Text += x.OuterXml + "\r\n";
+            }
         }
     }
 }
